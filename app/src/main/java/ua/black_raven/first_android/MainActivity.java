20ce7eb862cd7a3ff6394c7daa5a8b2b608a5a2f
Black_Raven92 <ukrop92@ukr.net>
2021-10-19 23:42:12 +0300

    Lesson2_calculate

--- app/src/main/java/ua/black_raven/first_android/MainActivity.java
+++ app/src/main/java/ua/black_raven/first_android/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,11 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculate_main);
+        Button button1 = findViewById(R.id.button1);
+        Button buttonClear = findViewById(R.id.button11);
+
+        EditText res = findViewById(R.id.textView);
+        button1.setOnClickListener(v->res.setText("1"));
+        buttonClear.setOnClickListener(v->res.setText("0"));
+
+
+
 
 
 
