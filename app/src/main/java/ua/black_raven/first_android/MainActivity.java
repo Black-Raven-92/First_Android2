1047fb01b4b5ca4da3fa616551359799c4123fe3
Black_Raven92 <ukrop92@ukr.net>
2021-10-19 19:00:21 +0300

    Lesson2_calculate

--- app/src/main/java/ua/black_raven/first_android/MainActivity.java
+++ app/src/main/java/ua/black_raven/first_android/MainActivity.java
@@ -3,13 +3,30 @@ package ua.black_raven.first_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private String name;
+    private String lastNAme;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.calculate_main);
+
+
+
+
+
+
+
 
     }
 }
